f1ab8accc23a4a2235a060addf1a12d16b3553b3
frontend33 <vikulov33@rambler.ru>
2018-09-21 10:34:49 +0300

    Добавил оптимизацию алгоритма, проверку на статус ответа с сервера , формирование списка из текстового файла построчно

--- parser_wms_layear.py
+++ parser_wms_layear.py
@@ -7,26 +7,42 @@ import requests
 x_min=5220000
 y_min=5200000
 
-list_coordinat=[[5220000,5200000],[5230000,5210000]]
+
+list=[]
+with open("C:/Users/azama//TILES/mygeoserver/newMahackala.txt", "r") as datafile:
+    for line in datafile:
+        featureline=line.replace('\t', ' ')
+        featureline=featureline.replace('\n', '')
+        # print(type(featureline))
+        feature_list = [int(featureline.split(" ")[0]), int(featureline.split(" ")[1]), int(featureline.split(" ")[0])+10000, int(featureline.split(" ")[1])+10000]
+        list.append(feature_list)
 count=0
-for coord in list_coordinat:
+
+for coord in list:
     count += 1
-    print count
     x_min=coord[0]
     y_min=coord[1]
-    x_max=x_min
-    y_max=y_min
+    x_max=coord[2]
+    y_max=coord[3]
     png_name ='my_server' + str(count)+".png"
-    pngw_name= 'my_server_' + str(count) + ".pngw"
-    x_max=x_max+20000
-    y_max=y_max+20000
+    pngw_name= 'my_server' + str(count) + ".pngw"
     link = 'https://localhost/geoserver/=' + str(
         x_min) + '%2C' + str(y_min) + '%2C' + str(x_max) + '%2C' + str(
         y_max) + 'transparent=true&f=image'
-    print link
-    urllib.urlretrieve(link, png_name)
+
+    # urllib.urlretrieve(link, png_name)
     # Берем ссылку
     r = requests.get(link)
+    # Выполнять функцию столько раз пока статус ответа не будет 200
+    while r.status_code !=200:
+        link = 'https://localhost/geoserver/=' + str(
+            x_min) + '%2C' + str(y_min) + '%2C' + str(x_max) + '%2C' + str(
+            y_max) + 'transparent=true&f=image'
+
+        r = requests.get(link)
+        print r.status_code
+
+
     # Записываем файл
     with open("C:/book/zho/"+ png_name, "wb") as code:
         code.write(r.content)
